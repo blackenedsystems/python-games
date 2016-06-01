9771bf183ba4bd2cfb6a32d27c33534dcd89aa94
BuildTools <unconfigured@null.spigotmc.org>
2016-06-01 21:51:43 +0200

    Enhance simple loop

--- scripts/simple_game_loop.py
+++ scripts/simple_game_loop.py
@@ -1,26 +1,44 @@
 import pygame
 
-# Initialise pygame
-pygame.init()
+# Define some colors as global constants
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
-# The window in which the game will run...
-size = (700, 500)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Simple Game Loop")
+def main():
+	# Initialise pygame
+	pygame.init()
 
-# Loop until the user clicks the close button.
-done = False
+	# The window in which the game will run...
+	size = (700, 500)
+	screen = pygame.display.set_mode(size)
+	
+	pygame.display.set_caption("Simple Game Loop")
 
-# Manages screen refresh rate
-clock = pygame.time.Clock()
-fps = 60
+	# Loop until the user clicks the close button.
+	done = False
 
-while not done:	
-	# Main event loop...
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:  # User pressed close, so end the script.
-			done = True
+	# Manages screen refresh rate
+	clock = pygame.time.Clock()
+	fps = 60
 
-	# Some game logic ....
+	while not done:	
+		# Main event loop...
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:  # User pressed close, so end the script.
+				done = True
 
-	clock.tick(fps)
+		# Clear the screen
+		screen.fill(WHITE)
+
+		# Draw stuff / Game Logic ...
+
+		# Update the screen
+		pygame.display.flip()
+
+		# Limit to fps frames per second
+		clock.tick(fps)
+
+if __name__ == "__main__":
+	main()		
